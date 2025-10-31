45e73392eed85b9c9433611532cb04f0b7e2c436
log1-codes <singhanurag1309@gmail.com>
2025-10-31 23:51:07 +0530

    made some utility files

--- Backend/app.js
+++ Backend/app.js
@@ -3,15 +3,16 @@ import cors from 'cors'
 import connectDB from "./src/config/DataBase.js";
 import pubmedRoutes from './src/routes/pubmedRoutes.js';
 import authRoutes from './src/routes/authRoutes.js'
+import profileRoutes from './src/routes/profileRoutes.js';
 const app = express();
 
-const allowedOrigins = ['https://project-lr-html.vercel.app']
+const allowedOrigins = ['http://127.0.0.1:5500', 'http://localhost:5500' ,'https://project-lr-html.vercel.app/index.html','https://project-lr-html.vercel.app'];
 connectDB();
 app.use(cors({
     origin: allowedOrigins,
     //  credentials : true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With']
 }))
 
 app.use(express.json()) ;
@@ -21,6 +22,7 @@ app.get("/" , (req,res)=>{
 
 app.use("/api/pubmed", pubmedRoutes);
 app.use("/api/auth" , authRoutes);
+app.use("/api", profileRoutes)
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({
---
+++ Backend/src/utils/constants.js
@@ -0,0 +1,2 @@
+export const JWT_SECRET = process.env.JWT_SECRET || "some seceret";
+export const SENDER_EMAIL = process.env.SENDER_EMAIL;
